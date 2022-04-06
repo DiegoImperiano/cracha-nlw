aea4d1b4f685e5b9cdf2b18a9a4173aa60f4bb90
Diego Imperiano <imperiano.cn@gmail.com>
2022-04-06 11:25:52 -0300

    criando comentario para explicar a função que consome api do github

--- main.js
+++ main.js
@@ -18,15 +18,15 @@ changeSocialMediaLinks()
 function getGitHubProfileInfos() {
   const url = `https://api.github.com/users/${LinksSocialMedia.github}`
 
-  fetch(url)
-    .then(response => response.json())
+  fetch(url) //função que pega o conteudo de uma url. nesse caso ele esta pegando as informações do arquivo JASON presente na url contida na variavel de mesmo nome
+    .then(response => response.json()) // .then é uma promeça e está pegando o conteudo retornado pela função fetch em um JSON
     .then(data => {
       userName.textContent = data.name
       userBio.textContent = data.bio
       userLink.href = data.html_url
       userImage.src = data.avatar_url
       userLogin.textContent = data.login
-    })
+    }) // essa outra promeça pega o arquivo ja em JSON e substitui as informações do html pelas informações do arquivo JSON
 }
 
 getGitHubProfileInfos()
